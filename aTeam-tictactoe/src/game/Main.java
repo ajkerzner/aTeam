b8346767d61d432ad2902ec7c0a2bb894f5ec274
Alex Kerzner <ajkerzner@smcm.edu>
2015-11-06 13:32:18 -0500

    Added formatting rules (hopefully)

--- aTeam-tictactoe/src/game/Main.java
+++ aTeam-tictactoe/src/game/Main.java
@@ -1,8 +1,12 @@
 package game;
 
-public class Main {
+public class Main
+{
 
-	public static void main(String[] args) {
+	public static void main(String[] args)
+	{
+		int[] array =
+			{ 1, 2, 3, 4, 5, };
 		// TODO Auto-generated method stub
 
 	}
