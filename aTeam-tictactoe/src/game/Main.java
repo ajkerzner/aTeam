c73890480e86844e66b53d7b7b1c421b32ffffd1
Alex Kerzner <ajkerzner@smcm.edu>
2015-11-11 18:05:15 -0500

    Normalization - end of file new line
    Added .gitattributes file, Will allow Git to end files with a single
    empty line.

--- aTeam-tictactoe/src/game/Main.java
+++ aTeam-tictactoe/src/game/Main.java
@@ -8,6 +8,9 @@ public class Main
 		int[] array =
 			{ 1, 2, 3, 4, 5, };
 		// TODO Auto-generated method stub
+		System.out.println("Hello world!");
+		System.out.println("This was added with Notepad++");
+		System.out.println("This was added with Eclipse");
 
 	}
 
