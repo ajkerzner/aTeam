dab788f2c1aba71c78762328034d60d74c6016e6
Alex Kerzner <ajkerzner@smcm.edu>
2015-11-11 18:53:15 -0500

    Added some sentences
    Tested with more stuff.

--- aTeam-tictactoe/src/game/Main.java
+++ aTeam-tictactoe/src/game/Main.java
@@ -8,10 +8,12 @@ public class Main
 		int[] array =
 			{ 1, 2, 3, 4, 5, };
 		// TODO Auto-generated method stub
-		System.out.println("Hello world!");
+		System.out.println("This was added with Atom!");
 		System.out.println("This was added with Notepad++");
 		System.out.println("This was added with Eclipse");
-
+		System.out.println("This was added with Vim");
+		System.out.println("This was added with Notepad (You know... Microsoft Windows Notepad)");
+		System.out.println("This was added with Sublime Text 2");
 	}
 
 }
