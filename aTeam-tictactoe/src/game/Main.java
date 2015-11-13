2d3b3d60b7f55c97a06f5dcd42f575f85d8b1c7c
Alex Kerzner <ajkerzner@smcm.edu>
2015-11-12 19:16:00 -0500

    Finalized preparation.

--- aTeam-tictactoe/src/game/Main.java
+++ aTeam-tictactoe/src/game/Main.java
@@ -5,19 +5,8 @@ public class Main
 
 	public static void main(String[] args)
 	{
-		int[] array =
-			{ 1, 2, 3, 4, 5, };
-		// TODO Auto-generated method stub
-		System.out.println("This was added with Atom!");
-		System.out.println("This was added with Notepad++");
-		System.out.println("This was added with Eclipse");
-		System.out.println("This was added with Vim");
-		System.out.println("This was added with Notepad (You know... Microsoft Windows Notepad)");
-		System.out.println("This was added with Sublime Text 2");
-		System.out.println("This was added with Visual Studio Code");
-
-		// This was pushed via Atom.
-		// This was committed with Visual Studio Code
+		//TODO Finish the project.
+		
 	}
 
 }
