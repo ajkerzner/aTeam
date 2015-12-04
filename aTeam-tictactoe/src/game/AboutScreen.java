858240ed73f7e422c3dcc7419a5c705d3eefef92
jsmiller <jsmiller@smcm.edu>
2015-12-04 00:01:30 -0500

    Fixed About Screen closing entire application on close, now disposes of the window. No main method in About/Name Screen anymore either

--- aTeam-tictactoe/src/game/AboutScreen.java
+++ aTeam-tictactoe/src/game/AboutScreen.java
@@ -27,7 +27,7 @@ public class AboutScreen extends javax.swing.JFrame {
       jButton1 = new javax.swing.JButton();
       setResizable(false);
 
-      setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+      setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
 
       jLabel1.setFont(new java.awt.Font("Tahoma", 1, 24)); // NOI18N
       jLabel1.setText("Tic-Tac-Toe");
