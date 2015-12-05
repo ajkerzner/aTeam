0f64a3398169d63d13f84350050b410e8158d739
Alex Kerzner <ajkerzner@smcm.edu>
2015-12-05 18:05:26 -0500

    Unnecessary commit, because Joe's back

--- aTeam-tictactoe/src/screens/MainScreen.java
+++ aTeam-tictactoe/src/screens/MainScreen.java
@@ -13,6 +13,9 @@ import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
 import javax.swing.KeyStroke;
+import game.Board;
+import game.Player;
+import game.Square;
 
 /**
  * 
@@ -20,23 +23,49 @@ import javax.swing.KeyStroke;
  * 
  * @author AlexKerzner
  * 
+ * @author Joe (original)
+ * 
  * 
  */
-public class mscreen extends JFrame
+public class MainScreen extends JFrame
 {
-
 	/**
+	 * An enumerated value representing the players
 	 * 
+	 * @author AlexKerzner
+	 *
 	 */
-	private static final long serialVersionUID = 1L;
-
-	/**
-	 * 
-	 */
-	public mscreen()
+	protected enum Turn
 	{
+		PLAYER_ONE,
+		PLAYER_TWO,
+		NO_PLAYERS
+	}
+
+	Turn											turn							= Turn.NO_PLAYERS;
+
+	protected Player					player_one;
+	protected Player					player_two;
+	protected Board						board;
+
+	protected Square					game_winner				= Square.EMPTY;
+
+	/**
+	 * Default serial version UID
+	 */
+	private static final long	serialVersionUID	= 1L;
+
+	/**
+	 * 
+	 */
+	public MainScreen()
+	{
+
 		// Set title
 		super("aTeam Tic-Tac-Toe");
+
+		// Creates board
+		board = new Board();
 
 		// Set default close operation
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -77,14 +106,13 @@ public class mscreen extends JFrame
 			KeyStroke.getKeyStroke(KeyEvent.VK_F4, ActionEvent.ALT_MASK));
 		// Add mnemonic
 		menu_exit.setMnemonic(KeyEvent.VK_E);
-		// Add action
+		// Add action to call exitGame(0)
 		menu_exit.addActionListener(new ActionListener()
 		{
-			@Override
 			public void actionPerformed(ActionEvent event)
 			{
 				// Exit normally
-				System.exit(0);
+				exitGame(0);
 			}
 		});
 		// Add to File menu
@@ -97,9 +125,48 @@ public class mscreen extends JFrame
 		this.setVisible(true);
 	}
 
+	/**
+	 * Exits the game
+	 * 
+	 * @param error_code
+	 *          the error code (default is 0)
+	 */
+	protected void exitGame(int error_code)
+	{
+		System.exit(error_code);
+	}
+
+	protected void newGame()
+	{
+		newGame(false);
+	}
+
+	/**
+	 * Start a new game
+	 * 
+	 * @param get_new_players
+	 *          true, if new players are to play
+	 */
+	protected void newGame(boolean get_new_players)
+	{
+		board = new Board();
+		// Get player names, if applicable
+		if (get_new_players)
+		{
+			// Call NameScreen
+			player_one = new Player();
+			player_two = new Player();
+		}
+
+		turn = Turn.PLAYER_ONE;
+
+	}
+
 	public static void main(String[] args)
 	{
-		mscreen screen = new mscreen();
+		// Suppressed because constructor does all the work
+		@SuppressWarnings("unused")
+		MainScreen screen = new MainScreen();
 	}
 
 }
